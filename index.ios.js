d601438f8ace5005a4c62e0204366b2f353892e1
Andrew Bestbier <andrew.bestbier@gmail.com>
2016-08-27 16:25:50 +0100

    Added eslint with Airbnb rules. Made listing recommendations

--- index.ios.js
+++ index.ios.js
@@ -1,71 +1,16 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import React from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
   View,
-  Image
 } from 'react-native';
 import NavigationBar from 'react-native-navbar';
-
-export class ListItem extends Component {
-  render() {
-
-    return (
-      <View style={styles.listItem}>
-        <View style={{flex: 5}}>
-        <Text style={styles.title}>
-          Buying Coffee from Starbucks
-        </Text>
-        <Text style={styles.subtitle}>
-          £90.30 saved
-        </Text>
-        </View>
-        <View style={styles.rightButton}>
-        </View>
-      </View>
-    );
-  }
-}
-
-class Resist extends Component {
-  render() {
-      var rightButtonConfig = {
-      title: 'Next',
-      handler: function onNext() {
-        alert('hello!');
-      }
-    };
-
-    return (
-      <View style={{flex: 1}} >
-        <NavigationBar
-          title={{title: 'Resist', tintColor: 'white'}}
-          leftButton={{title: 'Edit', tintColor: 'white'}}
-          rightButton={{title: 'New', tintColor: 'white'}}
-          tintColor='#4FA5F3'
-          style={styles.navbar}
-          />
-        <View style={styles.container}>
-          <ListItem />
-          <ListItem />
-          <ListItem />
-        </View>
-      </View>
-    );
-  }
-}
 
 const styles = StyleSheet.create({
   navbar: {
     borderBottomColor: '#ffffff',
-    borderBottomWidth: 1
+    borderBottomWidth: 1,
   },
   container: {
     flex: 1,
@@ -79,26 +24,57 @@ const styles = StyleSheet.create({
     borderLeftWidth: 7,
     flexDirection: 'row',
     marginTop: 10,
-    marginBottom: 10
+    marginBottom: 10,
   },
   title: {
     color: '#4A4A4A',
     paddingTop: 15,
-    paddingLeft: 15
+    paddingLeft: 15,
   },
   subtitle: {
     marginTop: 5,
     color: '#9B9B9B',
     paddingTop: 5,
     paddingLeft: 15,
-    paddingBottom: 15
+    paddingBottom: 15,
   },
   rightButton: {
     backgroundColor: '#C9E25D',
     flex: 1,
     borderTopRightRadius: 8,
     borderBottomRightRadius: 8,
-  }
+  },
 });
+
+const ListItem = () => (
+  <View style={styles.listItem}>
+    <View style={{ flex: 5 }}>
+      <Text style={styles.title}>
+        Buying Coffee from Starbucks
+      </Text>
+      <Text style={styles.subtitle}>
+        £90.30 saved
+      </Text>
+    </View>
+    <View style={styles.rightButton} />
+  </View>
+);
+
+const Resist = () => (
+  <View style={{ flex: 1 }} >
+    <NavigationBar
+      title={{ title: 'Resist', tintColor: 'white' }}
+      leftButton={{ title: 'Edit', tintColor: 'white' }}
+      rightButton={{ title: 'New', tintColor: 'white' }}
+      tintColor="#4FA5F3"
+      style={styles.navbar}
+    />
+    <View style={styles.container}>
+      <ListItem />
+      <ListItem />
+      <ListItem />
+    </View>
+  </View>
+);
 
 AppRegistry.registerComponent('Resist', () => Resist);
