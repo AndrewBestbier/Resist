a342e31f15aff1df85e5b74a933ebac496da64a7
Andrew Bestbier <andrew.bestbier@gmail.com>
2016-08-27 15:43:10 +0100

    Added static navbar and list item without image

--- index.ios.js
+++ index.ios.js
@@ -9,45 +9,83 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image
 } from 'react-native';
+import NavigationBar from 'react-native-navbar';
 
 class Resist extends Component {
   render() {
+      var rightButtonConfig = {
+      title: 'Next',
+      handler: function onNext() {
+        alert('hello!');
+      }
+    };
+
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to Resist
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      <View style={{flex: 1}} >
+        <NavigationBar
+          title={{title: 'Resist', tintColor: 'white'}}
+          leftButton={{title: 'Edit', tintColor: 'white'}}
+          rightButton={{title: 'New', tintColor: 'white'}}
+          tintColor='#4FA5F3'
+          style={styles.navbar}
+          />
+        <View style={styles.container}>
+          <View style={styles.listItem}>
+            <View style={{flex: 5}}>
+            <Text style={styles.title}>
+              Buying Coffee from Starbucks
+            </Text>
+            <Text style={styles.subtitle}>
+              £90.30 saved
+            </Text>
+            </View>
+            <View style={styles.rightButton}>
+            </View>
+          </View>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  navbar: {
+    borderBottomColor: '#ffffff',
+    borderBottomWidth: 1
+  },
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    padding: 20,
+    backgroundColor: '#4FA5F3',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  listItem: {
+    backgroundColor: 'white',
+    borderRadius: 8,
+    borderLeftColor: '#C9E25D',
+    borderLeftWidth: 7,
+    flexDirection: 'row'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  title: {
+    color: '#4A4A4A',
+    paddingTop: 15,
+    paddingLeft: 15
   },
+  subtitle: {
+    marginTop: 5,
+    color: '#9B9B9B',
+    paddingTop: 5,
+    paddingLeft: 15,
+    paddingBottom: 15
+  },
+  rightButton: {
+    backgroundColor: '#C9E25D',
+    flex: 1,
+    borderTopRightRadius: 8,
+    borderBottomRightRadius: 8,
+  }
 });
 
 AppRegistry.registerComponent('Resist', () => Resist);
